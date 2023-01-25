72a446aea82ee59d9b324e85396beeca87c8f988
coblin7 <commun7@naver.com>
2023-01-25 16:28:37 +0900

    feat: project

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = "벽돌 한 장";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <div>Coblog</div>
+      </div>
+      <div className='list'>        
+        <h3> { posts }</h3>
+        <p>1월 25일</p>
+        <hr/>
+      </div>
     </div>
   );
 }
