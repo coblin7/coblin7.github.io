665a26d5750c859862e5f08138a958b9916a4640
coblin7 <commun7@naver.com>
2023-03-10 13:51:01 +0900

    practice: route 적용

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import planetList from './page/planet.js';
 
@@ -15,7 +16,7 @@ function App() {
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Space Store</Navbar.Brand>
+          <Navbar.Brand href="#home">Space Shop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
@@ -23,21 +24,31 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      <br />
 
       <div className='main-bg' />
 
-      <div className='container'>
-        <div className='row'>
-          {
-            planetList.map( (planet, i)=>{
-              return (
-                <div className='col-md-4' key={i}><Planet planet={planet} /></div>
-              )
-            })
-          }
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<div>메인</div>} />
+        <Route path="/list" element={
+          <>
+            <div className='container'>
+              <div className='row'>
+                {
+                  planetList.map((planet, i) => {
+                    return (
+                      <Planet planet={planet} key={i} />
+                    )
+                  })
+                }
+              </div>
+            </div>
+          </>
+        } />
+        <Route path="/about" element={<div>상세</div>} />
+      </Routes>
+
+
+
     </div>
   );
 }
@@ -49,17 +60,20 @@ function App() {
  */
 function Planet(props) {
   let planet = props["planet"];
+
   return (
     <>
-    <div className='container'>
-      <div className='row'>
-        <div className='col-md-4 content'>
-          <img src={planet["img"]} width="80%" />
-          <h3><b>{planet["title"]}</b></h3>
-          <p>{planet["price"]}</p>
+      <div className='col-md-4'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-4 content'>
+              <img src={planet["img"]} width="80%" />
+              <h3><b>{planet["title"]}</b></h3>
+              <p>{planet["price"]}</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
